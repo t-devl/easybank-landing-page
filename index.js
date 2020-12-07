fd63061bf79918d2ab1027012704fb42e0818444
X <>
2020-12-07 13:27:29 +0000

    Prevent scrolling on mobile when nav is open

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const navToggle = document.querySelector(".nav__toggle");
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const navList = document.querySelector(".nav__list");
+const body = document.querySelector("body");
 
 navToggle.addEventListener("click", () => {
   if (navList.classList.contains("nav__list--open")) {
     navList.classList.remove("nav__list--open");
     navToggleIcon.src = "images/icon-hamburger.svg";
+    body.classList.remove("scroll-disabled");
   } else {
     navList.classList.add("nav__list--open");
     navToggleIcon.src = "images/icon-close.svg";
+    body.classList.add("scroll-disabled");
   }
 });
